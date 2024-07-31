d73b895d4fc1902912b99205b049c8a74f24cc22
atdmatdm <32565596+atdmatdm@users.noreply.github.com>
2024-08-01 01:40:42 +0300

    Create aesgen_var2.ps1

---
+++ aesgen_var2.ps1
@@ -0,0 +1,33 @@
+## Editing this file for best
+## run as admin  " Set-ExecutionPolicy -Scope CurrentUser RemoteSigned "
+
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("windows-1251")
+
+$random = [System.Security.Cryptography.RandomNumberGenerator]::Create();
+$datetime = Get-Date
+$outfilepatch = "./Aeskeys.txt"
+$inline = '------------------------------'
+Out-File -InputObject $datetime  -FilePath $outfilepatch 
+Out-File -InputObject $inline  -FilePath $outfilepatch -Append
+
+Write-Host "Введіть кількість ключів"  -ForegroundColor White -BackgroundColor Red
+$c = Read-Host
+Write-Host $inline
+function Get-Aeskey {
+	
+for($i=0; $i -lt $c; $i++){
+  $buffer = New-Object byte[] 32;
+  $random.GetBytes($buffer);
+  $key = [BitConverter]::ToString($buffer).Replace("-", [string]::Empty);
+  Write-Host "Key $($i): $($key)"
+  $Procs = "Key $($i): $($key)"
+  Out-File -InputObject $Procs  -FilePath $outfilepatch -Append
+ }
+ 
+}
+
+Get-Aeskey
+Out-File -InputObject $inline  -FilePath $outfilepatch -Append
+Write-Host $inline
+Write-Host "Пока" -ForegroundColor DarkGreen -BackgroundColor White
+CMD /c PAUSE
